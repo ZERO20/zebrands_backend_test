6a7f294259358dec85d7ca8fd31b8e4034771287
Edgar de la Cruz <edgar.cruz@xepelin.com>
2022-11-12 11:15:00 -0600

    feat: added Product Admin View

--- apps/products/admin.py
+++ apps/products/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Product
 
-# Register your models here.
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    '''Admin View for Product'''
+    list_display = ('name', 'sku', 'brand', 'price', 'created_at')
+    list_filter = ('created_at',)
+    readonly_fields = ('created_at', 'updated_at')
+    search_fields = ('name', 'sku', 'brand')
+    fields = ('name', 'sku', 'brand', 'price', 'active', 'created_at', 'updated_at')
+    ordering = ('created_at',)
