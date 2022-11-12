6cc292dfee8373f31281e667fd755401de62c34f
Edgar de la Cruz <edgar.cruz@xepelin.com>
2022-11-12 12:19:51 -0600

    feat: added health check view

--- apps/products/views.py
+++ apps/products/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+
+class HealthCheckAPIView(APIView):
+    """Health Check for the API"""
+
+    def get(self, request):
+        return Response(data={"status": "Hello world!"}, status=status.HTTP_200_OK)
