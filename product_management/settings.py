a2c89591ca014bacc4c431e27979b30e6efdbff4
Edgar de la Cruz <edgar.cruz@xepelin.com>
2022-11-14 11:12:45 -0600

    feat: config token lifetime

--- product_management/settings.py
+++ product_management/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -141,4 +142,9 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSIONS_CLASSES':(
         'rest_framework.permissions.IsAuthenticated'
     )
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
 }
