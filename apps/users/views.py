278a7c86f81525d41171bea30d680a6ee6500f0d
Edgar de la Cruz <edgar.cruz@xepelin.com>
2022-11-14 22:01:59 -0600

    ref: remove import

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,8 +1,5 @@
-
-from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.response import Response
-from rest_framework.viewsets import ModelViewSet, ViewSet
+from rest_framework.viewsets import ModelViewSet
 
 from django.contrib.auth.models import User
 
