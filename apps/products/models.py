e86cce0ce77fa5f96a4f5b2d13403696180e1f17
Edgar de la Cruz <edgar.cruz@xepelin.com>
2022-11-14 22:03:12 -0600

    feat: added detect changes and send notification

--- apps/products/models.py
+++ apps/products/models.py
@@ -1,4 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+from .constants import FIELDS_TO_REPORT
+from .email import SesEmail
 
 class Common(models.Model):
     """Base model for records"""
@@ -38,9 +42,48 @@ class Product(Common):
     def __str__(self):
         return self.name
 
+    def save(self, *args, **kw):
+        updated = False
+        data = {}
+        if self.pk is not None:
+            cls = self.__class__
+            old = cls.objects.get(pk=self.pk)
+            new = self
+            for field in cls._meta.get_fields():
+                field_name = field.name
+                if field_name in FIELDS_TO_REPORT \
+                    and getattr(old, field_name) != getattr(new, field_name):
+                    data[field_name] = getattr(new, field_name)
+                    updated = True
+        super(Product, self).save(*args, **kw)
+        if updated:
+            self.send_update_notification(changes=data)
+
     def tracking_detail(self):
         """Create a ProductTrack for the instance"""
         TrackProduct.objects.create(product_id=self.id)
+
+    def send_update_notification(self, changes):
+        """Send a update notification when a product is updated
+        Args:
+            changes dict: fields updated {'field': new_value}
+        """
+        admin_emails = list(User.objects.filter(is_superuser=True).values_list('email', flat=True))
+        subject = 'Product updated!'
+        html_content = f"""
+            <html>
+                <head></head>
+                <body>
+                <h1 style='text-align:center'>Product updated!</h1>
+                <h3>{self.name}</h3>
+                <strong>Changes:</strong>
+                <br>
+                {changes}
+                </body>
+            </html>
+        """
+        email = SesEmail(to=admin_emails, subject=subject, html_content=html_content)
+        email.send()
 
 class TrackProduct(Common):
     product = models.ForeignKey(Product, related_name="tracks", on_delete=models.CASCADE)
